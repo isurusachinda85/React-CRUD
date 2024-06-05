d1f7caac34d5d8a32404320c1ec49496c3a2bfe8
isurusachinda85 <isurusachintha8@gmail.com>
2024-06-06 03:35:31 +0530

    update app

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Hello</h1>
+        </div>
+      </div>
+    );
+  }
+}
