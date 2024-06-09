a8174cd6f15a3ffc118d8eaaa76a1be397628c9c
isurusachinda85 <isurusachintha8@gmail.com>
2024-06-10 02:05:39 +0530

    create customer getmethod

--- backend/routes/CustomerControllers.js
+++ backend/routes/CustomerControllers.js
@@ -3,7 +3,13 @@ const router = express.Router();
 const Customer = require("../models/Customer");
 
 router.get("/", async (req, res) => {
-  res.send("hello");
+  try {
+    const customer = await Customer.find();
+    res.json(customer);
+  } catch (error) {
+    console.error(e);
+    res.status(500).json({ error: "Something went wrong !" });
+  }
 });
 
 module.exports = router;
