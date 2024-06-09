68bb9fb06f52ff751fa656c7f86de85c5bf20486
isurusachinda85 <isurusachintha8@gmail.com>
2024-06-10 01:34:31 +0530

    update backend

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const customer = require("./routes/CustomerControllers");
 
 const app = express();
 
 const port = 4000;
 
 app.use(express.json());
+
+app.use("/customer", customer);
 
 app.listen(port, (req, res) => {
   console.log(`express app listening on port ${port}`);
