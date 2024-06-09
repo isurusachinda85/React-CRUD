81d782b4033063b60169b32bb7509c7725052d58
isurusachinda85 <isurusachintha8@gmail.com>
2024-06-10 02:35:33 +0530

    install material-ui

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ export default class Home extends Component {
   render() {
     return (
       <>
-        <div>
+        <div className="border border-black h-[100vh]">
           <h1 className="font-bold text-green-500 text-center">Hello</h1>
         </div>
       </>
