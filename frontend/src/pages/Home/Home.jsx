a5109e43ac7e7025baeedf26cf41e88bee0d4910
isurusachinda85 <isurusachintha8@gmail.com>
2024-06-10 03:03:22 +0530

    update frontend

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,44 @@
 import React, { Component } from "react";
+import TextField from "@mui/material/TextField";
 
 export default class Home extends Component {
   render() {
     return (
       <>
-        <div className="border border-black h-[100vh]">
-          <h1 className="font-bold text-green-500 text-center">Hello</h1>
+        <div className="border border-black h-[100vh] flex flex-col items-center justify-center ">
+          <div className="text-blue-600 text-3xl font-bold mb-5">
+            <h2>React CRUD</h2>
+          </div>
+
+          <div className=" w-[80%] h-[80%] shadow-2xl">
+            <div className="border border-black h-[10%] flex flex-row justify-around mt-5">
+              <div className="border border-black h-full w-[25%] flex items-center">
+                <TextField
+                  id="outlined-basic"
+                  label="ID"
+                  variant="outlined"
+                  size="small"
+                />
+                <div className="border border-black h-full w-[25%] "></div>
+              </div>
+              <div className="border border-black h-full w-[25%] flex items-center">
+                <TextField
+                  id="outlined-basic"
+                  label="Name"
+                  variant="outlined"
+                  size="small"
+                />
+              </div>
+              <div className="border border-black h-full w-[25%] flex items-center">
+                <TextField
+                  id="outlined-basic"
+                  label="Address"
+                  variant="outlined"
+                  size="small"
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </>
     );
