19ac43de05e6128fa51651eb3ef5b9a9dc641214
isurusachinda85 <isurusachintha8@gmail.com>
2025-01-25 00:48:31 +0530

    update

--- frontend/src/pages/ProductDetail.jsx
+++ frontend/src/pages/ProductDetail.jsx
@@ -1,10 +1,25 @@
 import React, { useEffect, useState } from "react";
 import ProductService from "../service/ProductService";
 import { useParams } from "react-router-dom";
-
+import { Button } from "@mui/material";
+import AddShoppingCartOutlinedIcon from "@mui/icons-material/AddShoppingCartOutlined";
 const ProductDetail = () => {
   const { id } = useParams();
   const [productData, setProductData] = useState([]);
+  const [number, setNumber] = useState(0);
+
+  const clickPluse = () => {
+    if (number < 5) {
+      setNumber(number + 1);
+    } else {
+      alert("maximum order " + number);
+    }
+  };
+  const clickMinus = () => {
+    if (number > 0) {
+      setNumber(number - 1);
+    }
+  };
 
   const viewProductDetails = async () => {
     let res = await ProductService.viewProductDetail(id);
@@ -32,25 +47,58 @@ const ProductDetail = () => {
             />
           </div>
 
-          <div className="w-[700px] h-[600px]  p-5 border border-black">
+          <div className="w-[700px] h-[60%] p-5 ">
             <h1 className="font-bold text-[45px] pb-2 mt-5">
               {data.description}
             </h1>
             <h1 className=" text-2xl text-[#09bafa] font-bold mt-5">
-              {data.price} <span className="text-black text-[15px]"> LKR</span>
+              <span className="text-black text-2xl"> LKR </span> {data.price}{" "}
+              .00
             </h1>
             <h1 className=" mt-5 text-[15px]">Category : {data.category}</h1>
             <h1 className=" mt-5 text-[15px]">Brand : {data.brand}</h1>
             <p className=" mt-5 font-bold">{data.phoneDetail}</p>
 
-            <div className="flex justify-center pt-6 gap-5 border border-black">
-              <button className="border border-[#09bafa] w-10  font-bold text-[#09bafa] rounded-md  hover:text-black hover:border-black">
+            <div className="flex p-5 ">
+              <button
+                className="border w-[40px] h-[40px] font-bold text-[#09bafa]  hover:text-black hover:border-black "
+                onClick={clickPluse}
+              >
                 +
               </button>
-              <h1>0</h1>
-              <button className="border border-[#09bafa] w-10  font-bold text-[#09bafa] rounded-md  hover:text-black hover:border-black">
+              <h1 className="border  w-[40px] h-[40px] flex justify-center items-center">
+                {number}
+              </h1>
+              <button
+                className="border w-[40px] h-[40px]  font-bold text-[#09bafa]   hover:text-black hover:border-black"
+                onClick={clickMinus}
+              >
                 -
               </button>
+            </div>
+            <div className="w-full flex flex-col gap-5 h-[100px] ">
+              <Button
+                className="h-[40px] w-56"
+                variant="contained"
+                style={{ background: "#09bafa" }}
+                endIcon={<AddShoppingCartOutlinedIcon />}
+              >
+                Add Cart
+              </Button>
+
+              <Button
+                className="h-[40px] w-56"
+                sx={{
+                  "&:hover": {
+                    backgroundColor: "#20bf6b",
+                    color: "white",
+                    borderColor: "#20bf6b",
+                  },
+                }}
+                variant="outlined"
+              >
+                Buy Now
+              </Button>
             </div>
           </div>
         </div>
--- frontend/src/pages/ProductList.jsx
+++ frontend/src/pages/ProductList.jsx
@@ -30,54 +30,47 @@ const ProductList = () => {
       </h1>
 
       <div className="flex flex-wrap justify-around  gap-10 p-10 mb-24">
-        {productData?.map((data, index) => {
-          //console.log(data._id);
-
-          if (data.brand === "Apple") {
-            return (
-              <Card
-                key={index}
-                variant="outlined"
-                className="h-[450px] w-[270px] shadow-md"
-              >
-                <Link to={`/productDetail/${data._id}`}>
-                  <div className="w-full h-[45%] flex items-center justify-center bg-[#f8f9fa] ">
-                    <img
-                      className="w-[150px] "
-                      src={`http://localhost:4000/` + data.image}
-                      alt=""
-                    />
-                  </div>
-                </Link>
-                <div className="w-full h-[55%] p-5 ">
-                  <div className="w-full h-[20%] font-bold text-[18px] flex items-center ">
-                    <h1>{data.name}</h1>
-                  </div>
-                  <div className="w-full h-[30%] flex items-center ">
-                    <h1>{data.description}</h1>
-                  </div>
-                  <div className="w-full h-[20%] text-[#09bafa] font-bold flex items-center ">
-                    <h1>
-                      {data.price}{" "}
-                      <span className="text-black text-[10px]">LKR</span>
-                    </h1>
-                  </div>
-                  <div className="w-full h-[30%] flex items-center">
-                    <Button
-                      className="h-[33px] w-56"
-                      variant="contained"
-                      style={{ background: "#09bafa" }}
-                      endIcon={<AddShoppingCartOutlinedIcon />}
-                    >
-                      Add Cart
-                    </Button>
-                  </div>
-                </div>
-              </Card>
-            );
-          }
-          return null;
-        })}
+        {productData?.map((data, index) => (
+          <Card
+            key={index}
+            variant="outlined"
+            className="h-[450px] w-[270px] shadow-md"
+          >
+            <Link to={`/productDetail/${data._id}`}>
+              <div className="w-full h-[45%] flex items-center justify-center bg-[#f8f9fa] ">
+                <img
+                  className="w-[150px] "
+                  src={`http://localhost:4000/` + data.image}
+                  alt=""
+                />
+              </div>
+            </Link>
+            <div className="w-full h-[55%] p-5 ">
+              <div className="w-full h-[20%] font-bold text-[18px] flex items-center ">
+                <h1>{data.name}</h1>
+              </div>
+              <div className="w-full h-[30%] flex items-center ">
+                <h1>{data.description}</h1>
+              </div>
+              <div className="w-full h-[20%] text-[#09bafa] font-bold flex items-center ">
+                <h1>
+                  {data.price}.00{" "}
+                  <span className="text-black text-[10px]"> LKR</span>
+                </h1>
+              </div>
+              <div className="w-full h-[30%] flex items-center">
+                <Button
+                  className="h-[33px] w-56"
+                  variant="contained"
+                  style={{ background: "#09bafa" }}
+                  endIcon={<AddShoppingCartOutlinedIcon />}
+                >
+                  Add Cart
+                </Button>
+              </div>
+            </div>
+          </Card>
+        ))}
       </div>
     </div>
   );
